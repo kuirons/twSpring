0d73d5ca04f36ea68b705484fd4f3b7021173533
kuirons <wugyu@outlook.com>
2018-01-18 01:30:29 +0800

    add:plane

---
+++ src/main/java/data/plane/Plane.java
@@ -0,0 +1,17 @@
+package data.plane;
+
+/**
+ * @Author: kuirons
+ * @Date: 18-1-18
+ */
+public class Plane {
+    private String planeId;
+
+    public String getPlaneId() {
+        return planeId;
+    }
+
+    public void setPlaneId(String planeId) {
+        this.planeId = planeId;
+    }
+}
